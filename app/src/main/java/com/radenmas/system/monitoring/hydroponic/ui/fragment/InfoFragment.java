0efe0fa935f0ac1e79eec6260c0589ab9bf880d2
Raden Mas <75772604+RadenMas6699@users.noreply.github.com>
2021-10-06 11:19:28 +0800

    Update Revision 1.2

--- app/src/main/java/com/radenmas/system/monitoring/hydroponic/ui/fragment/InfoFragment.java
+++ app/src/main/java/com/radenmas/system/monitoring/hydroponic/ui/fragment/InfoFragment.java
@@ -1,9 +1,13 @@
 package com.radenmas.system.monitoring.hydroponic.ui.fragment;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -11,6 +15,7 @@ import com.radenmas.system.monitoring.hydroponic.R;
 
 
 public class InfoFragment extends Fragment {
+    TextView app_version;
 
     public InfoFragment() {
     }
@@ -21,6 +26,20 @@ public class InfoFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_info_app, container, false);
 
+        Context context = getContext();
+
+        PackageManager manager = context.getPackageManager();
+        PackageInfo info = null;
+        try {
+            info = manager.getPackageInfo(
+                    context.getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        String version = info.versionName;
+
+        app_version = view.findViewById(R.id.app_version);
+        app_version.setText("Version " + version);
         return view;
     }
 }
