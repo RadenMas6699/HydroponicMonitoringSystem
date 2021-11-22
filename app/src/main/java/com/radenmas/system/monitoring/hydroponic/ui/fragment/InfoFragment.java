4fbb588f49024724221947da02336ec6076ecd1c
RadenMas6699 <radenmas.dev@gmail.com>
2021-11-22 11:11:31 +0800

    Update Revision 1.4

--- app/src/main/java/com/radenmas/system/monitoring/hydroponic/ui/fragment/InfoFragment.java
+++ app/src/main/java/com/radenmas/system/monitoring/hydroponic/ui/fragment/InfoFragment.java
@@ -3,6 +3,8 @@ package com.radenmas.system.monitoring.hydroponic.ui.fragment;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.graphics.text.LineBreaker;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,7 +17,7 @@ import com.radenmas.system.monitoring.hydroponic.R;
 
 
 public class InfoFragment extends Fragment {
-    TextView app_version;
+    TextView app_version, desc_app;
 
     public InfoFragment() {
     }
@@ -40,6 +42,13 @@ public class InfoFragment extends Fragment {
 
         app_version = view.findViewById(R.id.app_version);
         app_version.setText("Version " + version);
+
+        desc_app = view.findViewById(R.id.desc_app);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            desc_app.setJustificationMode(LineBreaker.JUSTIFICATION_MODE_INTER_WORD);
+        }
+
+
         return view;
     }
 }
